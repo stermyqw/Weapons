ace30eea0f45d40871e46be11a89c36042fd8a7c
1811 <1811@DESKTOP-P6PO77Q>
2024-12-19 22:30:12 +0700

    Добавьте файлы проекта.

---
+++ Weapons/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Weapons
+{
+
+    public abstract class Weapon
+    {
+        public abstract int Damage { get; }
+        public abstract string Attack();
+        public abstract string Description();
+    }
+
+    public class Sword : Weapon
+    {
+        public override int Damage => 15;
+
+        public override string Attack()
+        {
+            return $"Вы нанесли {Damage} урона мечом.";
+        }
+
+        public override string Description()
+        {
+            return "Это меч.";
+        }
+    }
+
+    public class Bow : Weapon
+    {
+        public override int Damage => 10;
+
+        public override string Attack()
+        {
+            return $"Вы выстрелили из лука и нанесли {Damage} урона.";
+        }
+
+        public override string Description()
+        {
+            return "Это лук.";
+        }
+    }
+
+    public class Staff : Weapon
+    {
+        public override int Damage => 20;
+
+        public override string Attack()
+        {
+            return $"Вы использовали магический посох и нанесли {Damage} урона.";
+        }
+
+        public override string Description()
+        {
+            return "Это посох.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon sword = new Sword();
+            Weapon bow = new Bow();
+            Weapon staff = new Staff();
+
+            Console.WriteLine(sword.Attack());
+            Console.WriteLine(sword.Description());
+
+            Console.WriteLine(bow.Attack());
+            Console.WriteLine(bow.Description());
+
+            Console.WriteLine(staff.Attack());
+            Console.WriteLine(staff.Description());
+        }
+    }
+
+}
---
+++ Weapons/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Weapons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Weapons")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("491641da-7fb6-4f44-854b-c5db6a782471")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
